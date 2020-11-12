00e7e4e626c4729c9ad0762153d34195733b129e
MasatoShima <masato.shima@quick.jp>
2020-11-12 10:53:28 +0900

    Updated

--- consumer.py
+++ consumer.py
@@ -26,7 +26,7 @@ from confluent_kafka.avro.serializer import SerializerError
 # **************************************************
 HOST = "10.2.152.95"
 PORT = "9092"
-TOPIC = "SKDB.public.sdcocdmst"
+TOPIC = "SKDB.public.sdmcobilccl"
 
 DIR_OUTPUT = "data/output/"
 
